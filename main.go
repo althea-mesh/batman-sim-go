82331f512644ce900118732dd0bde314f85e4acf
Jehan <jehan.tremback@gmail.com>
2017-03-25 16:54:06 -0700

    experiment with saturation instead of bucket

--- main.go
+++ main.go
@@ -1,202 +1,284 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"log"
+	"sync"
+	"time"
 )
 
-const (
-	timeMultiplier = 1000
-	hopPenalty     = 15 / 255
-)
+func main() {
+	ch := make(chan int)
+	go listen(ch)
+	go send(ch, 4)
+	time.Sleep(time.Second)
+	go send(ch, 1)
+	time.Sleep(time.Second)
+	go send(ch, 1)
+	time.Sleep(time.Second)
+	go send(ch, 1)
+	time.Sleep(time.Second)
+	go send(ch, 1)
+	time.Sleep(time.Second)
+	go send(ch, 1)
 
-type Network struct {
-	Nodes map[string]*Node
-	Edges map[string]*Edge
+	time.Sleep(time.Minute)
 }
 
-type Node struct {
-	*Network
-	Address       string
-	Originators   map[string]*Originator
-	Neighbors     map[string]*Neighbor
-	OgmSequence   int
-	PacketChannel chan (Packet)
+func send(ch chan int, i int) {
+	ch <- i
 }
 
-type Neighbor struct {
-	Address    string
-	Throughput int
+type Link struct {
+	sat bool
+	mut *sync.Mutex
 }
 
-type Originator struct {
-	Address string
-	NextHop struct {
-		Address    string
-		Throughput int
+func MakeLink() Link {
+	return Link{false, &sync.Mutex{}}
+}
+
+func (l *Link) Saturate(i int) {
+	l.mut.Lock()
+	l.sat = true
+	l.mut.Unlock()
+
+	time.Sleep(time.Duration(i) * time.Second)
+
+	l.mut.Lock()
+	l.sat = false
+	l.mut.Unlock()
+}
+
+func (l *Link) IsSaturated() bool {
+	l.mut.Lock()
+	sat := l.sat
+	l.mut.Unlock()
+
+	return sat
+}
+
+func listen(ch chan int) {
+	l := MakeLink()
+	for {
+		i := <-ch
+		if !l.IsSaturated() {
+			go l.Saturate(i)
+			log.Println("keeping", i)
+		} else {
+			log.Println("discarding", i)
+		}
 	}
-	OgmSequence int
 }
 
-type Edge struct {
-	Throughput int
-	Bucket     int
-}
+// package main
 
-type OGM struct {
-	Sequence          int
-	OriginatorAddress string
-	SenderAddress     string
-	Throughput        int
-	Timestamp         int
-}
+// import (
+// 	"encoding/json"
+// 	"errors"
+// 	"log"
+// 	"time"
+// )
 
-type Packet struct {
-	Type    string
-	Payload []byte
-}
+// const (
+// 	timeMultiplier = 1000
+// 	hopPenalty     = 15 / 255
+// )
 
-// func (router *Node) BroadcastOgm() {
-// 	router.OgmSequence++
-// 	for address, _ := range router {
-// 		router.
+// type Network struct {
+// 	Nodes map[string]*Node
+// 	Edges map[string]*Edge
+// }
+
+// type Node struct {
+// 	*Network
+// 	Address       string
+// 	Originators   map[string]*Originator
+// 	Neighbors     map[string]*Neighbor
+// 	OgmSequence   int
+// 	PacketChannel chan (Packet)
+// }
+
+// type Neighbor struct {
+// 	Address    string
+// 	Throughput int
+// }
+
+// type Originator struct {
+// 	Address string
+// 	NextHop struct {
+// 		Address    string
+// 		Throughput int
+// 	}
+// 	OgmSequence int
+// }
+
+// type Edge struct {
+// 	Throughput     int
+// 	Bucket         int
+// 	PacketChannel  chan (Packet)
+// 	TicksPerSecond int
+// 	Destination    *Node
+// }
+
+// type OGM struct {
+// 	Sequence          int
+// 	OriginatorAddress string
+// 	SenderAddress     string
+// 	Throughput        int
+// 	Timestamp         int
+// }
+
+// type Packet struct {
+// 	Type    string
+// 	Payload []byte
+// }
+
+// // func (router *Node) BroadcastOgm() {
+// // 	router.OgmSequence++
+// // 	for address, _ := range router {
+// // 		router.
+// // 	}
+// // }
+
+// func (edge *Edge) SendBits(num int) bool {
+// 	newBucket := edge.Bucket + num
+// 	if newBucket > edge.Throughput {
+// 		edge.Bucket = edge.Throughput
+// 		return false
+// 	}
+// 	edge.Bucket = newBucket
+// 	return true
+// }
+
+// func (edge *Edge) RunTicker() {
+// 	for range time.Tick(time.Second / time.Duration(edge.TicksPerSecond)) {
+// 		if edge.Bucket > 0 {
+// 			edge.Bucket -= edge.Throughput / edge.TicksPerSecond
+// 		}
 // 	}
 // }
 
-func (edge *Edge) SendBits(num int) bool {
-	newBucket := edge.Bucket + num
-	if newBucket > edge.Throughput {
-		edge.Bucket = edge.Throughput
-		return false
-	}
-	edge.Bucket = newBucket
-	return true
-}
+// func (edge *Edge) Listen() {
+// 	for {
+// 		packet := <-edge.PacketChannel
+// 		if edge.SendBits(len(packet.Payload)) {
+// 			edge.Destination.PacketChannel <- packet
+// 		}
+// 	}
+// }
 
-func (edge *Edge) Tick(ticksPerSecond int) {
-	if edge.Bucket > 0 {
-		edge.Bucket -= edge.Throughput / ticksPerSecond
-	}
-}
+// func (node *Node) SendPacket(destAddress string, packetType string, payload []byte) error {
+// 	edge := node.Network.Edges[node.Address+"->"+destAddress]
+// 	if edge == nil {
+// 		return errors.New("edge not found")
+// 	}
 
-func (node *Node) SendPacket(destAddress string, packetType string, payload []byte) error {
-	dest := node.Network.Nodes[destAddress]
-	if dest == nil {
-		return errors.New("destination not found")
-	}
+// 	if edge.SendBits(len(payload)) {
+// 		edge.PacketChannel <- Packet{packetType, payload}
+// 	}
 
-	edge := node.Network.Edges[node.Address+"->"+dest.Address]
-	if dest == nil {
-		return errors.New("edge not found")
-	}
+// 	return nil
+// }
 
-	if edge.SendBits(len(payload)) {
-		dest.ReceivePacket(Packet{packetType, payload})
-	}
+// func (node *Node) Listen() {
+// 	for {
+// 		packet := <-node.PacketChannel
+// 		var err error
+// 		switch packet.Type {
+// 		case "OGM":
+// 			err = node.HandleOGM(packet.Payload)
+// 		}
 
-	return nil
-}
+// 		if err != nil {
+// 			log.Println(node.Address, err)
+// 		}
+// 	}
+// }
 
-func (node *Node) Listen() {
-	for {
-		packet := <-node.PacketChannel
-		var err error
-		switch packet.Type {
-		case "OGM":
-			err = node.HandleOGM(packet.Payload)
-		}
+// func (node *Node) HandleOGM(payload []byte) error {
+// 	ogm := OGM{}
+// 	err := json.Unmarshal(payload, ogm)
+// 	if err != nil {
+// 		return err
+// 	}
 
-		if err != nil {
-			log.Println(node.Address, err)
-		}
-	}
-}
+// 	if ogm.OriginatorAddress == node.Address {
+// 		return nil
+// 	}
 
-func (node *Node) HandleOGM(payload []byte) error {
-	ogm := OGM{}
-	err := json.Unmarshal(payload, ogm)
-	if err != nil {
-		return err
-	}
+// 	adjustedOGM, err := node.AdjustOGM(ogm)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	if ogm.OriginatorAddress == node.Address {
-		return nil
-	}
+// 	err = node.UpdateOriginator(*adjustedOGM)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	adjustedOGM, err := node.AdjustOGM(ogm)
-	if err != nil {
-		return err
-	}
+// 	node.RebroadcastOGM(*adjustedOGM)
+// 	return nil
+// }
 
-	err = node.UpdateOriginator(*adjustedOGM)
-	if err != nil {
-		return err
-	}
+// func (node *Node) AdjustOGM(ogm OGM) (*OGM, error) {
+// 	/* Update the received throughput metric to match the link
+// 	 * characteristic:
+// 	 *  - If this OGM traveled one hop so far (emitted by single hop
+// 	 *    neighbor) the path throughput metric equals the link throughput.
+// 	 *  - For OGMs traversing more than one hop the path throughput metric is
+// 	 *    the smaller of the path throughput and the link throughput.
+// 	 */
+// 	neighbor := node.Neighbors[ogm.SenderAddress]
+// 	if neighbor == nil {
+// 		return nil, errors.New("OGM not sent from neighbor")
+// 	}
 
-	node.RebroadcastOGM(*adjustedOGM)
-	return nil
-}
+// 	if ogm.OriginatorAddress == ogm.SenderAddress {
+// 		ogm.Throughput = neighbor.Throughput
+// 	} else {
+// 		if neighbor.Throughput < ogm.Throughput {
+// 			ogm.Throughput = neighbor.Throughput
+// 		}
+// 	}
 
-func (node *Node) AdjustOGM(ogm OGM) (*OGM, error) {
-	/* Update the received throughput metric to match the link
-	 * characteristic:
-	 *  - If this OGM traveled one hop so far (emitted by single hop
-	 *    neighbor) the path throughput metric equals the link throughput.
-	 *  - For OGMs traversing more than one hop the path throughput metric is
-	 *    the smaller of the path throughput and the link throughput.
-	 */
-	neighbor := node.Neighbors[ogm.SenderAddress]
-	if neighbor == nil {
-		return nil, errors.New("OGM not sent from neighbor")
-	}
+// 	ogm.Throughput = ogm.Throughput - (ogm.Throughput * hopPenalty)
+// 	return &ogm, nil
+// }
 
-	if ogm.OriginatorAddress == ogm.SenderAddress {
-		ogm.Throughput = neighbor.Throughput
-	} else {
-		if neighbor.Throughput < ogm.Throughput {
-			ogm.Throughput = neighbor.Throughput
-		}
-	}
+// func (node *Node) UpdateOriginator(ogm OGM) error {
+// 	originator := node.Originators[ogm.OriginatorAddress]
 
-	ogm.Throughput = ogm.Throughput - (ogm.Throughput * hopPenalty)
-	return &ogm, nil
-}
+// 	if originator == nil {
+// 		originator := Originator{
+// 			OgmSequence: ogm.Sequence,
+// 			Address:     ogm.OriginatorAddress,
+// 		}
+// 		originator.NextHop.Address = ogm.SenderAddress
+// 		originator.NextHop.Throughput = ogm.Throughput
 
-func (node *Node) UpdateOriginator(ogm OGM) error {
-	originator := node.Originators[ogm.OriginatorAddress]
+// 		node.Originators[ogm.OriginatorAddress] = &originator
+// 	} else if ogm.Sequence > originator.OgmSequence {
+// 		if originator.NextHop.Throughput < ogm.Throughput {
+// 			originator.NextHop.Address = ogm.SenderAddress
+// 			originator.NextHop.Throughput = ogm.Throughput
+// 		}
+// 	} else {
+// 		return errors.New("ogm sequence too low")
+// 	}
+// 	return nil
+// }
 
-	if originator == nil {
-		originator := Originator{
-			OgmSequence: ogm.Sequence,
-			Address:     ogm.OriginatorAddress,
-		}
-		originator.NextHop.Address = ogm.SenderAddress
-		originator.NextHop.Throughput = ogm.Throughput
-
-		node.Originators[ogm.OriginatorAddress] = &originator
-	} else if ogm.Sequence > originator.OgmSequence {
-		if originator.NextHop.Throughput < ogm.Throughput {
-			originator.NextHop.Address = ogm.SenderAddress
-			originator.NextHop.Throughput = ogm.Throughput
-		}
-	} else {
-		return errors.New("ogm sequence too low")
-	}
-	return nil
-}
-
-func (node *Node) RebroadcastOGM(ogm OGM) error {
-	ogm.SenderAddress = node.Address
-	payload, err := json.Marshal(ogm)
-	if err != nil {
-		return err
-	}
-	for _, neighbor := range node.Neighbors {
-		err = node.SendPacket(neighbor.Address, "OGM", payload)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
+// func (node *Node) RebroadcastOGM(ogm OGM) error {
+// 	ogm.SenderAddress = node.Address
+// 	payload, err := json.Marshal(ogm)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	for _, neighbor := range node.Neighbors {
+// 		err = node.SendPacket(neighbor.Address, "OGM", payload)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
