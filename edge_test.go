fc88ce000fa2c68da91e981cb167de54d595805a
Jehan <jehan.tremback@gmail.com>
2017-04-06 17:38:18 -0700

    first test works

--- edge_test.go
+++ edge_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
@@ -18,53 +17,34 @@ func TestSaturate(t *testing.T) {
 	}
 
 	go edge.SendPacket(hundredKb)
-
-	// 	select {
-	// 	case <-time.After(time.Millisecond * 1):
-	// 		t.Fatalf("Packet should have succeeded.")
-	// 	case <-pChan:
-	// 	}
-
-	// 	go edge.SendPacket(hundredKb)
-
-	// 	select {
-	// 	case <-time.After(time.Millisecond * 1):
-	// 	case <-pChan:
-	// 		t.Fatalf("Packet should have been dropped.")
-	// 	}
-
-	// 	time.Sleep(time.Millisecond * 1805)
-	// 	go edge.SendPacket(hundredKb)
-
-	// 	select {
-	// 	case <-time.After(time.Millisecond * 10):
-	// 		t.Fatalf("Packet should have succeeded.")
-	// 	case <-pChan:
-	// }
-
-	timer := time.Now()
-L:
-	for {
-		go edge.SendPacket(hundredKb)
-		select {
-		default:
-		case <-pChan:
-			fmt.Println(time.Since(timer))
-			// if time.Since(timer) > time.Millisecond*805 ||
-			// 	time.Since(timer) < time.Millisecond*795 {
-			// 	t.Fatalf("Not in time range 1.5s - 1.7s %v", time.Since(timer))
-			// }
-			break L
-		}
+	select {
+	case <-time.After(time.Millisecond * 1):
+		t.Fatalf("Packet should have succeeded.")
+	case <-pChan:
 	}
 
-	// // time.Sleep(time.Millisecond * 2)
-	// go edge.SendPacket(hundredKb)
-	// select {
-	// case <-time.After(time.Millisecond * 1):
-	// case <-pChan:
-	// 	t.Fatalf("Packet should have been dropped.")
-	// }
+	go edge.SendPacket(hundredKb)
+	select {
+	case <-time.After(time.Millisecond * 1):
+	case <-pChan:
+		t.Fatalf("Packet should have been dropped.")
+	}
+
+	time.Sleep(time.Millisecond * 1805)
+
+	go edge.SendPacket(hundredKb)
+	select {
+	case <-time.After(time.Millisecond * 10):
+		t.Fatalf("Packet should have succeeded.")
+	case <-pChan:
+	}
+
+	go edge.SendPacket(hundredKb)
+	select {
+	case <-time.After(time.Millisecond * 10):
+	case <-pChan:
+		t.Fatalf("Packet should have been dropped.")
+	}
 }
 
 // 	timer := time.Now()
