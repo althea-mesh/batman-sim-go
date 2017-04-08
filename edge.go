a91d3546dac2de8f0532f77689017304afafc806
Jehan <jehan.tremback@gmail.com>
2017-04-07 23:39:42 -0700

    much more efficient edge

--- edge.go
+++ edge.go
@@ -1,43 +1,24 @@
 package main
 
-import (
-	"sync"
-	"time"
-)
+import "time"
+
+const quantize = 10000
 
 type Edge struct {
 	Throughput    int
 	PacketChannel *chan (Packet)
-	sat           bool
-	mut           sync.Mutex
-}
-
-func (edge *Edge) saturate(bytes int) {
-	edge.mut.Lock()
-	edge.sat = true
-	edge.mut.Unlock()
-
-	bits := bytes*8 + 20
-	satDuration := time.Duration(bits*(1000000/edge.Throughput)) * time.Microsecond
-
-	time.Sleep(satDuration)
-
-	edge.mut.Lock()
-	edge.sat = false
-	edge.mut.Unlock()
-}
-
-func (edge *Edge) isSaturated() bool {
-	edge.mut.Lock()
-	sat := edge.sat
-	edge.mut.Unlock()
-
-	return sat
+	lastPacket    PacketRecord
 }
 
 func (edge *Edge) SendPacket(packet Packet) {
-	if !edge.isSaturated() {
-		go edge.saturate(len(packet.Payload))
+	bits := edge.lastPacket.Bytes * 8
+	satDuration := time.Duration(bits*1000/edge.Throughput) * time.Millisecond
+
+	if time.Since(edge.lastPacket.Time) > satDuration {
 		*edge.PacketChannel <- packet
+		edge.lastPacket = PacketRecord{
+			Time:  time.Now(),
+			Bytes: len(packet.Payload),
+		}
 	}
 }
--- edge_test.go
+++ edge_test.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"testing"
 	"time"
 )
 
-func TestSaturate(t *testing.T) {
+func TestSaturateTime(t *testing.T) {
+	log.SetFlags(log.Lmicroseconds)
+	pChan := make(chan (Packet))
+	edge := Edge{
+		Throughput:    1000 * 1000, // 1 megabit
+		PacketChannel: &pChan,
+	}
+
+	hundredKb := Packet{
+		Payload: make([]byte, 1000*100),
+	}
+
+	go edge.SendPacket(hundredKb)
+	<-pChan
+	timer := time.Now()
+L:
+	for {
+		go edge.SendPacket(Packet{})
+		select {
+		default:
+		case <-pChan:
+			fmt.Println(time.Since(timer))
+			break L
+		}
+	}
+}
+
+func TestSaturateBasic(t *testing.T) {
 	pChan := make(chan (Packet))
 	edge := Edge{
 		Throughput:    1000 * 1000, // 1 megabit
@@ -18,35 +47,66 @@ func TestSaturate(t *testing.T) {
 
 	go edge.SendPacket(hundredKb)
 	select {
-	case <-time.After(time.Millisecond * 1):
+	case <-time.After(10 * time.Millisecond):
 		t.Fatalf("Packet should have succeeded.")
 	case <-pChan:
 	}
 
 	go edge.SendPacket(hundredKb)
 	select {
-	case <-time.After(time.Millisecond * 1):
+	case <-time.After(10 * time.Millisecond):
 	case <-pChan:
 		t.Fatalf("Packet should have been dropped.")
 	}
 
-	time.Sleep(time.Millisecond * 805)
+	time.Sleep(time.Millisecond * 905)
 
 	go edge.SendPacket(hundredKb)
 	select {
-	case <-time.After(time.Millisecond * 10):
+	case <-time.After(10 * time.Millisecond):
 		t.Fatalf("Packet should have succeeded.")
 	case <-pChan:
 	}
+}
 
-	time.Sleep(time.Millisecond * 795)
+func TestSaturateProbability(t *testing.T) {
+	successChan := make(chan (bool))
+	numTries := 100
 
-	go edge.SendPacket(hundredKb)
-	select {
-	case <-time.After(time.Millisecond * 10):
-	case <-pChan:
-		t.Fatalf("Packet should have been dropped.")
+	for i := 0; i < numTries; i++ {
+		go func() {
+			pChan := make(chan (Packet))
+			edge := Edge{
+				Throughput:    1000 * 1000, // .5 megabit
+				PacketChannel: &pChan,
+			}
+
+			hundredKb := Packet{
+				Payload: make([]byte, 1000*100),
+			}
+
+			go edge.SendPacket(hundredKb)
+			<-pChan
+			time.Sleep(time.Millisecond * 800)
+
+			go edge.SendPacket(hundredKb)
+			select {
+			case <-time.After(1 * time.Millisecond):
+				successChan <- false
+			case <-pChan:
+				successChan <- true
+			}
+		}()
 	}
+
+	var numSucceeded int
+	for i := 0; i < numTries; i++ {
+		if <-successChan {
+			numSucceeded++
+		}
+	}
+
+	fmt.Println(numSucceeded)
 }
 
 // 	timer := time.Now()
